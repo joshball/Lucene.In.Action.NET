803ab2555fb5220b36b047e76cf8fab7b7a8516d
Joshua Ball <joshua.g.s.ball@gmail.com>
2014-08-07 13:22:56 -0700

    Adding TestApps

---
+++ TestApps/IndexDump/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Lucene.Net.Analysis.Standard;
+using LuceneHelpers;
+
+namespace IndexDump
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length != 1)
+            {
+                Console.WriteLine("Usage: IndexDump <indexDir>");
+                return;
+            }
+
+            var indexDir = args[0];
+            Console.WriteLine("Directory for new Lucene index: [{0}]", indexDir);
+            try
+            {
+                var version = Lucene.Net.Util.Version.LUCENE_30;
+                var analyzer = new StandardAnalyzer(Lucene.Net.Util.Version.LUCENE_30);
+                var tester = new LuceneFileDirectoryTester(indexDir, analyzer, version);
+                tester.CheckIndex();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
+
+        }
+    }
+}
---
+++ TestApps/Searcher/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Lucene.Net.Analysis.Standard;
+using Lucene.Net.Documents;
+using LuceneHelpers;
+using Directory = System.IO.Directory;
+
+namespace Searcher
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length != 2)
+            {
+                Console.WriteLine("Usage: Searcher <indexDir> query");
+                return;
+            }
+
+            var indexDir = args[0];
+            var query = args[1];
+
+            var stopwatch = new Stopwatch();
+            stopwatch.Start();
+
+            try
+            {
+                var version = Lucene.Net.Util.Version.LUCENE_30;
+                var analyzer = new StandardAnalyzer(Lucene.Net.Util.Version.LUCENE_30);
+                var tester = new LuceneFileDirectoryTester(indexDir, analyzer, version);
+                var hits = tester.Search(query);
+                stopwatch.Stop();
+                Console.WriteLine("Search.hits.MaxScore: {0}", hits.MaxScore);
+                Console.WriteLine("Search.hits.TotalHits: {0}", hits.TotalHits);
+                Console.WriteLine("Search.hits.ScoreDocs: {0}", hits.ScoreDocs.Length);
+                Console.WriteLine("Searcher found {0} documents (in {1} ms) that matched the query.", hits.TotalHits, stopwatch.Elapsed);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
+
+        }
+    }
+}
