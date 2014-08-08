a2aa0c34b488a7dd480694b5af0a9f4137f2a423
Joshua Ball <joshua.g.s.ball@gmail.com>
2014-08-08 14:32:45 -0700

    Cleaned up the basics.

--- ChapterTests/03/BasicSearchTests.cs
+++ ChapterTests/03/BasicSearchTests.cs
@@ -1,25 +1,19 @@
-﻿using System;
-using Lucene.Net.Documents;
+﻿using Lucene.Net.Analysis;
 using Lucene.Net.Index;
+using Lucene.Net.QueryParsers;
 using Lucene.Net.Search;
 using Lucene.Net.Store;
-using LuceneHelpers;
 using Xunit;
+using LuceneVersion = Lucene.Net.Util.Version;
 
 namespace ChapterTests._03
 {
-    public class BasicSearchTests
+    public class BasicSearchTests : BasicSearchTestsBase
     {
-        protected TestEnvironment TestEnvironment { get; set; }
-        public BasicSearchTests()
-        {
-            TestEnvironment = new TestEnvironment();
-        }
 
         [Fact]
         public void TestTermSearch()
         {
-            //            var dir = TestUtil.getBookIndexDirectory(); //A
             using (var dir = FSDirectory.Open(TestEnvironment.TestIndexDirectory))
             using (var indexSearcher = new IndexSearcher(dir))
             {
@@ -35,16 +29,8 @@ namespace ChapterTests._03
                 var termSubjectJUnit = new Term("subject", "junit");
                 var termQuerySubjectJUnit = new TermQuery(termSubjectJUnit);
                 var topDocsSubjectJUnit = indexSearcher.Search(termQuerySubjectJUnit, 10);
-                
-//                foreach (var result in topDocsSubjectJUnit.ScoreDocs)
-//                {
-//                    var explanation = indexSearcher.Explain(termQuerySubjectJUnit, result.Doc);
-//                    Console.WriteLine("Explanation of Doc: {0}\n {1}", result.Doc, explanation.ToString());
-//                    var document = indexSearcher.Doc(result.Doc, new MapFieldSelector("title"));
-//                    var title = document.Get("title");
-//                    var subject = document.Get("subject");
-//                    Console.WriteLine("Document: {0} title: {1} || subject: {2}", result.Doc, title, subject);
-//                }
+
+//                ExplainResults(indexSearcher, termQuerySubjectJUnit, topDocsSubjectJUnit);
 
                 // title=JUnit in Action, Second Edition
                 // subject=junit unit testing mock objects
@@ -56,7 +42,62 @@ namespace ChapterTests._03
 
         }
 
+        /// <summary>
+        /// Search for a specific term (smallest indexed piece). In this case, ISBN.
+        /// Terms are case sensitive.
+        /// Terms are Good for getting docs by key.
+        /// if indexed with NOT_ANALYZED, then same value can be used.
+        /// NOT_ANALYZED != unique
+        /// 
+        /// </summary>
+        [Fact]
+        public void TestKeywordSearch()
+        {
+            using (var dir = FSDirectory.Open(TestEnvironment.TestIndexDirectory))
+            using (var indexSearcher = new IndexSearcher(dir))
+            {
+                var term = new Term("isbn", "9781935182023");
+                var termQuery = new TermQuery(term);
+                var topDocs = indexSearcher.Search(termQuery, 10);
+                Assert.Equal(1, topDocs.TotalHits);
+            }
+            
+        }
 
+        [Fact]
+        public void TestQueryParser()
+        {
+            using (var dir = FSDirectory.Open(TestEnvironment.TestIndexDirectory))
+            using (var indexSearcher = new IndexSearcher(dir))
+            {
+                var queryParser = new QueryParser(LuceneVersion.LUCENE_30, "contents", new SimpleAnalyzer());
+                var query = queryParser.Parse("+JUNIT +ANT -MOCK");
 
+                var topDocs = indexSearcher.Search(query, 10);
+                Assert.Equal(1, topDocs.TotalHits);
+
+                var document = indexSearcher.Doc(topDocs.ScoreDocs[0].Doc);
+                Assert.Equal("Ant in Action", document.Get("title"));
+
+                var queryTwo = queryParser.Parse("mock OR junit");
+                var topDocsTwo = indexSearcher.Search(queryTwo, 10);
+                // "Ant in Action, JUnit in Action, Second Edition"
+                Assert.Equal(2, topDocsTwo.TotalHits);
+            }
+            
+        }
+
+        [Fact]
+        public void TestTermRangeQuery()
+        {
+            using (var dir = FSDirectory.Open(TestEnvironment.TestIndexDirectory))
+            using (var indexSearcher = new IndexSearcher(dir))
+            {
+                var termRangeQuery = new TermRangeQuery(field:"title2", lowerTerm: "d", upperTerm:"j",includeLower:true, includeUpper:true);
+                var topDocs = indexSearcher.Search(termRangeQuery, 100);
+                Assert.Equal(3, topDocs.TotalHits);
+            }
+            
+        }
     }
 }
