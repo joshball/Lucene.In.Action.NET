147860aa50d437a76f12dde445f6ebbe0d0c6405
Joshua Ball <joshua.g.s.ball@gmail.com>
2014-08-10 21:44:47 -0700

    Wildcard test.

--- ChapterTests/03/QueryParserTest.cs
+++ ChapterTests/03/QueryParserTest.cs
@@ -17,6 +17,25 @@ namespace ChapterTests._03
         [Fact]
         public void TestWildcards()
         {
+            using (var directory = new RAMDirectory())
+            {
+                IndexSingleFieldDocs(directory, new[] { 
+                    new Field("contents", "wild", Field.Store.YES, Field.Index.ANALYZED),
+                    new Field("contents", "child", Field.Store.YES, Field.Index.ANALYZED),
+                    new Field("contents", "mild", Field.Store.YES, Field.Index.ANALYZED),
+                    new Field("contents", "mildew", Field.Store.YES, Field.Index.ANALYZED) 
+                });
+
+                using (var indexSearcher = new IndexSearcher(directory))
+                {
+                    var query = new WildcardQuery(new Term("contents", "?ild*"));
+                    var topDocs = indexSearcher.Search(query, 10);
+                    Assert.Equal(3, topDocs.TotalHits); // "child no match"
+                    Assert.Equal(topDocs.ScoreDocs[0].Score, topDocs.ScoreDocs[1].Score, 1); // "score the same"
+                    Assert.Equal(topDocs.ScoreDocs[1].Score, topDocs.ScoreDocs[2].Score, 1); // "score the same"
+                }
+            }
+
         }
 
     }
