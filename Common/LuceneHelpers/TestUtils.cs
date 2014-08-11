70151fd86be4fbf27cf4e8aa2f4bac89f490ab76
Joshua Ball <joshua.g.s.ball@gmail.com>
2014-08-10 21:00:50 -0700

    Added unicode helper

--- Common/LuceneHelpers/TestUtils.cs
+++ Common/LuceneHelpers/TestUtils.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.IO;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using Lucene.Net.Documents;
 using Lucene.Net.Search;
@@ -22,6 +24,18 @@ namespace LuceneHelpers
             // foreach (var row in File.ReadAllLines(filename))
             //      properties.Add(row.Split('=')[0], string.Join("=", row.Split('=').Skip(1).ToArray()));
             return ConvertLinesToProperties(File.ReadAllLines(filename));
+        }
+
+        // borrowed from: http://stackoverflow.com/a/1615860/39758
+        static string DecodeEncodedNonAsciiCharacters(string value)
+        {
+            return Regex.Replace(
+                value,
+                @"\\u(?<Value>[a-zA-Z0-9]{4})",
+                m =>
+                {
+                    return ((char)int.Parse(m.Groups["Value"].Value, NumberStyles.HexNumber)).ToString();
+                });
         }
 
         public static bool HitsIncludeTitle(IndexSearcher searcher, TopDocs topDocs, String title)
