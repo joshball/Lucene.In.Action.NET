c9723b5cc91e84624bf581952692ce0ea1bfbc77
Joshua Ball <joshua.g.s.ball@gmail.com>
2014-08-10 21:27:48 -0700

    Adding QueryParser Tests

---
+++ ChapterTests/03/QueryParserTest.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChapterTests._03
+{
+    public class QueryParserTest : BasicSearchTestsBase
+    {
+
+    }
+}
